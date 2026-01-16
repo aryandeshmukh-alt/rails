d8785da81ff76f8066bc154a201e2d831a82d06d
Aryan Padmakar Deshmukh <aryan.deshmukh@joshsoftware.com>
2026-01-16 17:26:22 +0530

    Handle record not found for blog show action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :publish]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   PER_PAGE = 5
 
   def index
@@ -9,13 +11,21 @@ class BlogsController < ApplicationController
 
     @blogs = Blog.published
     @blogs = @blogs.where("title LIKE ?", "%#{params[:search]}%") if params[:search].present?
-
     @blogs = @blogs.offset((page - 1) * PER_PAGE).limit(PER_PAGE)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @blogs }
+    end
 
     @current_page = page
   end
 
   def show
+    respond_to do |format|
+      format.html
+      format.json { render json: @blog }
+    end
   end
 
   def new
@@ -61,5 +71,12 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.require(:blog).permit(:title, :body)
+  end
+
+  def record_not_found
+    respond_to do |format|
+      format.html { redirect_to blogs_path, alert: "Blog not found." }
+      format.json { render json: { error: "Blog not found" }, status: :not_found }
+    end
   end
 end
